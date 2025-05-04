30d800f5625256eb6d626849182ddc84bbe2b990
rajendra0968jangid <rajendra0968jangid@gmail.com>
2025-05-05 00:23:50 +0530

    Initial commit

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,8 @@
 import axios from "axios";
+const Url = import.meta.env.VITE_URL;
+console.log(Url);
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api";
+const BASE_URL = import.meta.env.MODE === "development" ? Url : "/api";
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
